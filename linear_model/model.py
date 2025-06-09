cf7e561955a314140c4d4dd0d31180e50ab353f7
kg5kb8lbj6 <1658422703@qq.com>
2025-06-09 21:40:05 +0800

    linear model

--- linear_model/model.py
+++ linear_model/model.py
@@ -7,4 +7,34 @@
 @Version :   python3
 @Contact :   1658422730@qq.com
 '''
+import numpy as np
+from tqdm import tqdm
+from utils import load_data, init_params, linear_loss
+import matplotlib.pyplot as plt
 
+
+
+class LinearModel:
+    def __init__(self, learning_rate = 0.01 , n_iterations = 10000):
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
+        self.weights = None
+        self.bias = None
+    def fit(self, x, y):
+        his_loss, epochs = [], []
+        # w, b都是参数
+        w, b  = init_params(x.shape[1]) 
+        for i in tqdm(range(self.n_iterations), desc = "train linear model"):
+            y_hat, loss, dw, db = linear_loss(x, y, w, b)
+            w += -self.learning_rate * dw
+            b += -self.learning_rate * db
+            his_loss.append(loss)
+            epochs.append(i)
+            param = {'w': w, 'b': b}
+            grad = {"dw": dw, "db": db}
+        return his_loss, epochs, param, grad
+if __name__ == "__main__":
+    train_data, train_target, test_data, test_target = load_data()
+    model = LinearModel(learning_rate = 0.01, n_iterations = 10000)
+    his_loss, epochs, param, grad = model.fit(train_data, train_target)
+    print(his_loss[:20])
---
+++ linear_model/utils.py
@@ -0,0 +1,59 @@
+#! python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   utils.py
+@Time    :   2025/06/09 20:46:14
+@Author  :   Liu ZhongFei
+@Version :   python3
+@Contact :   1658422730@qq.com
+'''
+import numpy as np
+from sklearn.datasets import load_diabetes
+from sklearn.utils import shuffle
+
+def load_data():
+    """
+        load data
+    """
+    diabetes = load_diabetes()
+    data, target  = shuffle(diabetes.data, diabetes.target, random_state=42)
+    train_data, train_target = data[:int(len(data) * 0.8)], target[:int(len(target) * 0.8)]
+    test_data, test_target = data[int(len(data) * 0.8):], target[int(len(target) * 0.8):]
+    train_target = train_target.reshape(-1, 1)
+    test_target = test_target.reshape(-1, 1)
+    assert train_data.shape[0] == train_target.shape[0] and test_data.shape[0] == test_target.shape[0]
+    return train_data, train_target, test_data, test_target
+
+
+def init_params(dim):
+    """
+        初始化参数
+    """
+    w = np.zeros((dim, 1))
+    b = 0
+    return w, b
+
+def linear_loss(data, target, w, b):
+    n_sample = data.shape[0]
+    y_hat = np.dot(data, w) + b
+    loss = np.sum((y_hat - target) ** 2) / n_sample
+    dw = np.dot(data.T, (y_hat - target))
+    db = np.sum((y_hat - target)) / n_sample
+    return y_hat, loss, dw, db
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    train_data, train_target, test_data, test_target = load_data()
+    w, b = init_params(train_data.shape[1])
+    # y_hat, loss, dw, db = linear_loss(train_data, train_target, w, b)
+    print("Train data shape:", train_data.shape)
+    print("Train target shape:", train_target.shape)
+    print("Test data shape:", test_data.shape)
+    print("Test target shape:", test_target.shape)
+    print(f"params w shape:{w.shape}")
