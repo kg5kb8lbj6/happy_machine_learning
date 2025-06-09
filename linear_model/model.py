449d79dca10bd42115be0cf9c84747e739f95fe2
kg5kb8lbj6 <1658422703@qq.com>
2025-06-09 20:00:41 +0800

    增加model的头注释

---
+++ linear_model/model.py
@@ -0,0 +1,10 @@
+#! python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   model.py
+@Time    :   2025/06/09 20:00:19
+@Author  :   Liu ZhongFei
+@Version :   python3
+@Contact :   1658422730@qq.com
+'''
+
